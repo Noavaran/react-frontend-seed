d5dc0acf846e0a7a478004bba4cd38cf951ef88f
seyedreza67 <s.hosseyni2013@gmail.com>
2017-12-18 20:08:58 +0330

    add icon and button component

---
+++ src/components/common/Button/Button.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import { Button as ButtonMaterial } from 'material-ui';
+import PropTypes from 'prop-types';
+
+export default class Button extends Component {
+
+    static propTypes = {
+        text: PropTypes.string,
+        color: PropTypes.oneOf(["default","inherit","primary","accent","contrast"]),
+        disabled: PropTypes.bool,
+        raised: PropTypes.bool,
+        onClick: PropTypes.func,
+        href: PropTypes.string
+    };
+
+    static defaultProps = {
+        color: 'primary',
+        raised: true
+    };
+
+    handleClick = () => {
+        if(this.props.onClick) {
+            this.props.onClick();
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <ButtonMaterial
+                    raised={this.props.raised}
+                    color={this.props.color}
+                    disabled={this.props.disabled}
+                    onClick={this.handleClick}
+                    href={this.props.href}
+                    className="ButtonMaterialClass">
+                        {this.props.children}
+                        {this.props.text}
+                </ButtonMaterial>
+            </div>
+        )
+    }
+}
